1bebb0b228a8c214f387802e0fba6a022c303c76
Subdev514 <bhattaraid514@gmail.com>
2026-06-04 13:36:24 +0545

    Tokens

--- include/chithi/common.h
+++ include/chithi/common.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #define CHITHI_VERSION "0.1.0"
+#include <stdint.h>
---
+++ include/chithi/token_type.h
@@ -0,0 +1,29 @@
+#include "span.h"
+#include <stdint.h>
+
+enum TOKEN_TYPE{
+	// Language keywords
+	TK_LET, TK_LOOP, TK_IF, TK_ELIF, TK_ELSE, TK_OR, TK_AND, TK_RETURN, TK_SAY, TK_BREAK, TK_DEFINE,
+	
+	// Symbols
+	TK_EQUALS, TK_DOUBLE_EQUALS, TK_DOT, TK_DOT_DOT, TK_LEFT_BRACE, TK_RIGHT_BRACE, TK_LEFT_BRACKET,
+	TK_RIGHT_BRACKET, TK_PLUS, TK_MINUS, TK_DIVIDE, TK_PERCENTAGE, TK_GRATERTHAN, TK_GREATERTHAN_EQUAL, TK_LESSTHAN, TK_LESSTHAN_EQUAL,
+
+	// Litreals
+	TK_IDENTIFIER, TK_STRING_LITREAL, TK_DIGIT_LITREAL, TK_VOID_LITREAL, TK_OBJECT_LITREAL, TK_FALSE, TK_TRUE,
+	
+	TK_EOF
+};
+
+typedef struct Token{
+    TOKEN_TYPE type;
+    Span span;
+	char* lexeme;
+	union litreal
+	{
+		uint64_t i64;
+		double d;
+	};
+	
+
+} TOKEN;
